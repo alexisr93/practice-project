d9e89a5827b3c60ed88dcb024b8ea5190fddc319
Alexis Ramirez <aramirez@bitwiseindustries.com>
2022-04-04 16:02:01 -0700

    fix(client): move react-redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
-import { store } from './store';
 import { Provider } from 'react-redux';
+import { store } from './store';
 import 'antd/dist/antd.css';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={store}>
